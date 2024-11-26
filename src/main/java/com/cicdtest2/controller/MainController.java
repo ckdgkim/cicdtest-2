ca17b4ff81e8e4a23e09d77140c5ebc5684f7b62
kostackdgk <kostackdgkim@naver.com>
2024-11-26 16:25:21 +0900

    src commit

---
+++ src/main/java/com/cicdtest2/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.cicdtest2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/")
+    public String main() {
+        return "this is cicdtest2 자동배포";
+    }
+}
---
+++ src/test/java/com/cicdtest2/Cicdtest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicdtest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicdtest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
